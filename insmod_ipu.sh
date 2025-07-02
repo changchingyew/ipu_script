248e88d1ec01baf53d298e5def4931ff928f2a03
Yew, Chang Ching <chang.ching.yew@intel.com>
2025-07-03 02:44:46 +0800

    Add misc scripts

---
+++ insmod_ipu.sh
@@ -0,0 +1,11 @@
+insmod ./ipu-bridge.ko
+insmod ./ipu6-acpi-common.ko 
+insmod ./ipu6-acpi-pdata.ko 
+insmod ./ipu6-acpi.ko
+insmod ./intel-ipu6.ko
+insmod ./intel-ipu6-isys.ko
+
+insmod ./serdes.ko
+insmod ./max9296.ko
+insmod ./max9295.ko
+insmod ./isx031.ko
---
+++ kgdb.sh
@@ -0,0 +1,2 @@
+echo ttyS0 > /sys/module/kgdboc/parameters/kgdboc
+echo g > /proc/sysrq-trigger
