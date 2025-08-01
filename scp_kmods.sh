0534941858412a8205a9a94472368baf9ddd8243
Yew, Chang Ching <chang.ching.yew@intel.com>
2025-08-02 03:24:10 +0800

    Loop kernel module copy ops

--- scp_kmods.sh
+++ scp_kmods.sh
@@ -8,14 +8,31 @@ export SSH_PASSWORD="user1234"
 if [ "$1" == "lts" ]; then
     echo "Using LTS kernel build"
     export SCP_TARGET_DIR=/usr/lib/modules/6.12.34-vtg-gmsl/kernel
-    sshpass -p $SSH_PASSWORD scp $KERNEL_DIR/drivers/media/i2c/max9x/*.ko $SCP_TARGET:$SCP_TARGET_DIR/drivers/media/i2c/max9x/
 else
     echo "Using non-LTS kernel build"
     export SCP_TARGET_DIR=/usr/lib/modules/6.12.13-maxim-serdes/kernel
     sshpass -p $SSH_PASSWORD scp $KERNEL_DIR/arch/x86/boot/bzImage $SCP_TARGET:/boot/vmlinuz-6.12.13-maxim-serdes
-    sshpass -p $SSH_PASSWORD scp $KERNEL_DIR/drivers/media/i2c/maxim-serdes/*.ko $SCP_TARGET:$SCP_TARGET_DIR/drivers/media/i2c/maxim-serdes/
 fi
 
-sshpass -p $SSH_PASSWORD scp $KERNEL_DIR/drivers/media/i2c/isx031.ko $SCP_TARGET:$SCP_TARGET_DIR/drivers/media/i2c/
-# sshpass -p $SSH_PASSWORD scp $KERNEL_DIR/drivers/i2c/i2c-atr.ko $SCP_TARGET:$SCP_TARGET_DIR/drivers/i2c/
-sshpass -p $SSH_PASSWORD scp $KERNEL_DIR/drivers/i2c/i2c-mux.ko $SCP_TARGET:$SCP_TARGET_DIR/drivers/i2c/
+declare -a modules=(\
+    "isx031" \
+    "i2c-atr" \
+    "v4l2*"\
+    )
+declare -a dirs=(\
+    "drivers/media/i2c" \
+    "drivers/i2c" \
+    "drivers/media/v4l2-core" \
+    )
+
+if [ "$1" == "lts" ]; then
+    modules+=("*")
+    dirs+=("drivers/media/i2c/max9x")
+else
+    modules+=("max*")
+    dirs+=("drivers/media/i2c/maxim-serdes")
+fi
+
+for i in "${!modules[@]}"; do
+    sshpass -p $SSH_PASSWORD scp $KERNEL_DIR/${dirs[$i]}/${modules[$i]}.ko $SCP_TARGET:$SCP_TARGET_DIR/${dirs[$i]}/
+done
