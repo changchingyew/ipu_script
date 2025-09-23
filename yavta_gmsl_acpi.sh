b1104b92f5f80ec39a44409e1d1ddd6e7d197bb4
Yew, Chang Ching <chang.ching.yew@intel.com>
2025-09-24 06:38:45 +0800

    max96724 yavta 2x isx031 stream

--- yavta_gmsl_acpi.sh
+++ yavta_gmsl_acpi.sh
@@ -10,11 +10,13 @@ if [ "$1" = "ipu6epmtl" ]; then
 elif [ "$1" = "ipu6ep" ]; then
     PRT1=1
     PRT2=2
-elif [ "$1" = "ipu7ep" ]; then
+elif [ "$1" = "ipu7" ]; then
     PRT1=2
     DES1="max96724 2-0027"
-    SER1="max96717 2-0040"
-    SEN1="isx031 2-001a"
+    SER1="max96717 30-0040"
+    SER2="max96717 31-0040"
+    SEN1="isx031 34-001a"
+    SEN2="isx031 35-001a"
     IPU="Intel IPU7"
 else
     echo "invalid platform!"
@@ -39,19 +41,22 @@ ascii_base_value=$(printf "%d" "'a")
 CSI1="${IPU} CSI2 ${PRT1}"
 # CSI2="${IPU} CSI2 ${PRT2}"
 CAP1="${IPU} ISYS Capture $((PRT1 * 8))"
-# CAP2="${IPU} ISYS Capture $((PRT1 * 8 + 1))"
+CAP2="${IPU} ISYS Capture $((PRT1 * 8 + 1))"
 # CAP3="${IPU} ISYS Capture $((PRT2 * 8))"
 # CAP4="${IPU} ISYS Capture $((PRT2 * 8 + 1))"
 CAPTURE_DEV1=$(media-ctl -e "${CAP1}")
-# CAPTURE_DEV2=$(media-ctl -e "${CAP2}")
+CAPTURE_DEV2=$(media-ctl -e "${CAP2}")
 # CAPTURE_DEV3=$(media-ctl -e "${CAP3}")
 # CAPTURE_DEV4=$(media-ctl -e "${CAP4}")
 
 echo "SEN1:" $SEN1
+echo "SEN2:" $SEN2
 echo "SER1:" $SER1
+echo "SER2:" $SER2
 echo "DES1:" $DES1
 echo "CSI1:" $CSI1
 echo "CAP1:" $CAP1
+echo "CAP2:" $CAP2
 
 # declare -p
 # media-ctl -r
@@ -67,60 +72,59 @@ echo "CAP1:" $CAP1
 # media-ctl -l "\"${DES1}\":0 -> \"${CSI1}\":0[1]"
 # media-ctl -l "\"${DES2}\":0 -> \"${CSI2}\":0[1]"
 media-ctl -l "\"${CSI1}\":1 -> \"${CAP1}\":0[1]"
-# media-ctl -l "\"${CSI1}\":2 -> \"${CAP2}\":0[1]"
+media-ctl -l "\"${CSI1}\":2 -> \"${CAP2}\":0[1]"
 # media-ctl -l "\"${CSI2}\":1 -> \"${CAP3}\":0[1]"
 # media-ctl -l "\"${CSI2}\":2 -> \"${CAP4}\":0[1]"
 # media-ctl -R "\"${SER1}\" [0/0->2/0[1]]"
 # media-ctl -R "\"${SER2}\" [0/0->2/1[1]]"
 # media-ctl -R "\"${SER3}\" [0/0->2/0[1]]"
 # media-ctl -R "\"${SER4}\" [0/0->2/1[1]]"
-# media-ctl -R "\"${DES1}\" [4/0->0/0[1],5/1->0/1[1]]"
+
+media-ctl -R "\"${DES1}\" [0/0->4/0[1],1/0->4/1[1]]"
 # media-ctl -R "\"${DES2}\" [4/0->0/0[1],5/1->0/1[1]]"
-# media-ctl -R "\"${CSI1}\" [0/0->1/0[1],0/1->2/1[1]]"
+media-ctl -R "\"${CSI1}\" [0/0->1/0[1],0/1->2/0[1]]"
 # media-ctl -R "\"${CSI2}\" [0/0->1/0[1],0/1->2/1[1]]"
 media-ctl -V "\"${SEN1}\":0 [fmt:${FMT1}/${RESOLUTION}]"
-# media-ctl -V "\"${SEN2}\":0 [fmt:${FMT1}/${RESOLUTION}]"
+media-ctl -V "\"${SEN2}\":0 [fmt:${FMT1}/${RESOLUTION}]"
 # media-ctl -V "\"${SEN3}\":0 [fmt:${FMT1}/${RESOLUTION}]"
 # media-ctl -V "\"${SEN4}\":0 [fmt:${FMT1}/${RESOLUTION}]"
-media-ctl -V "\"${SER1}\":0/0 [fmt:${FMT1}/${RESOLUTION} field:none]"
 
-# media-ctl -V "\"${SER1}\":2/0 [fmt:${FMT1}/${RESOLUTION}]"
-media-ctl -V "\"${SER1}\":1/0 [fmt:${FMT1}/${RESOLUTION} field:none]"
+media-ctl -V "\"${SER1}\":0/0 [fmt:${FMT1}/${RESOLUTION}]"
+media-ctl -V "\"${SER1}\":1/0 [fmt:${FMT1}/${RESOLUTION}]"
 
-# media-ctl -V "\"${SER2}\":0/0 [fmt:${FMT1}/${RESOLUTION}]"
-# media-ctl -V "\"${SER2}\":2/1 [fmt:${FMT1}/${RESOLUTION}]"
+media-ctl -V "\"${SER2}\":0/0 [fmt:${FMT1}/${RESOLUTION}]"
+media-ctl -V "\"${SER2}\":1/0[fmt:${FMT1}/${RESOLUTION}]"
 # media-ctl -V "\"${SER3}\":0/0 [fmt:${FMT1}/${RESOLUTION}]"
 # media-ctl -V "\"${SER3}\":2/0 [fmt:${FMT1}/${RESOLUTION}]"
 # media-ctl -V "\"${SER4}\":0/0 [fmt:${FMT1}/${RESOLUTION}]"
 # media-ctl -V "\"${SER4}\":2/1 [fmt:${FMT1}/${RESOLUTION}]"
 
 media-ctl -V "\"${DES1}\":4/0 [fmt:${FMT1}/${RESOLUTION}]"
-# media-ctl -V "\"${DES1}\":2/0 [fmt:${FMT1}/${RESOLUTION} field:none]"
+media-ctl -V "\"${DES1}\":4/1 [fmt:${FMT1}/${RESOLUTION}]"
 
-media-ctl -V "\"${DES1}\":0/0 [fmt:${FMT1}/${RESOLUTION} field:none]"
-# media-ctl -V "\"${DES1}\":5/1 [fmt:${FMT1}/${RESOLUTION}]"
-# media-ctl -V "\"${DES1}\":0/1 [fmt:${FMT1}/${RESOLUTION}]"
+media-ctl -V "\"${DES1}\":0/0 [fmt:${FMT1}/${RESOLUTION}]"
+media-ctl -V "\"${DES1}\":1/0 [fmt:${FMT1}/${RESOLUTION}]"
+
 # media-ctl -V "\"${DES2}\":4/0 [fmt:${FMT1}/${RESOLUTION}]"
 # media-ctl -V "\"${DES2}\":0/0 [fmt:${FMT1}/${RESOLUTION}]"
 # media-ctl -V "\"${DES2}\":5/1 [fmt:${FMT1}/${RESOLUTION}]"
 # media-ctl -V "\"${DES2}\":0/1 [fmt:${FMT1}/${RESOLUTION}]"
 media-ctl -V "\"${CSI1}\":0/0 [fmt:${FMT1}/${RESOLUTION}]"
+media-ctl -V "\"${CSI1}\":0/1 [fmt:${FMT1}/${RESOLUTION}]"
 media-ctl -V "\"${CSI1}\":1/0 [fmt:${FMT1}/${RESOLUTION}]"
-# media-ctl -V "\"${CSI1}\":0/1 [fmt:${FMT1}/${RESOLUTION}]"
-# media-ctl -V "\"${CSI1}\":2/1 [fmt:${FMT1}/${RESOLUTION}]"
+media-ctl -V "\"${CSI1}\":2/0 [fmt:${FMT1}/${RESOLUTION}]"
 # media-ctl -V "\"${CSI2}\":0/0 [fmt:${FMT1}/${RESOLUTION}]"
 # media-ctl -V "\"${CSI2}\":1/0 [fmt:${FMT1}/${RESOLUTION}]"
 # media-ctl -V "\"${CSI2}\":0/1 [fmt:${FMT1}/${RESOLUTION}]"
 # media-ctl -V "\"${CSI2}\":2/1 [fmt:${FMT1}/${RESOLUTION}]"
 
-echo 0xff > /sys/class/video4linux/$(basename $CAPTURE_DEV1)/dev_debug
+# echo 0xff > /sys/class/video4linux/$(basename $CAPTURE_DEV1)/dev_debug
 yavta -f$FMT -s${RESOLUTION} --no-query "${CAPTURE_DEV1}"
-# yavta -f$FMT -s${RESOLUTION} --no-query "${CAPTURE_DEV2}"
+yavta -f$FMT -s${RESOLUTION} --no-query "${CAPTURE_DEV2}"
 # yavta -f$FMT -s${RESOLUTION} --no-query "${CAPTURE_DEV3}"
 # yavta -f$FMT -s${RESOLUTION} --no-query "${CAPTURE_DEV4}"
 
-# yavta --data-prefix -c50 -n5 -I -s1920x1536 --file=./dump/frame-#-gsml-a.bin -f UYVY /dev/video0
- yavta --data-prefix -c${FRAME_NUM} -n5 -I -s${RESOLUTION} --file=./dump/frame-#-gsml-a.bin -f $FMT ${CAPTURE_DEV1}
-# yavta --data-prefix -c${FRAME_NUM} -n5 -I -s${RESOLUTION} --file=./dump/frame-#-gsml-b.bin -f $FMT ${CAPTURE_DEV2} &
+# yavta --data-prefix -c${FRAME_NUM} -n5 -I -s${RESOLUTION} --file=./dump/frame-#-gsml-a.bin -f $FMT ${CAPTURE_DEV1}
+yavta --data-prefix -c${FRAME_NUM} -n5 -I -s${RESOLUTION} --file=./dump/frame-#-gsml-b.bin -f $FMT ${CAPTURE_DEV2}
 # yavta --data-prefix -c${FRAME_NUM} -n5 -I -s${RESOLUTION} --file=./dump/frame-#-gsml-c.bin -f $FMT ${CAPTURE_DEV3} &
 # yavta --data-prefix -c${FRAME_NUM} -n5 -I -s${RESOLUTION} --file=./dump/frame-#-gsml-d.bin -f $FMT ${CAPTURE_DEV4} &
