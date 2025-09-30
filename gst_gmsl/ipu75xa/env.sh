19549ce99f3e421891ba699945dd579419bd5e27
Yew, Chang Ching <chang.ching.yew@intel.com>
2025-09-30 09:46:29 +0800

    gst isx031 streaming with max96724

---
+++ gst_gmsl/ipu75xa/env.sh
@@ -0,0 +1,10 @@
+export GST_PLUGIN_PATH=/usr/lib/gstreamer-1.0
+export GST_VIDEO_SINK=waylandsink
+ 
+# export cameraDebug=3
+ 
+# export cameraDump=0x101
+# export cameraDumpPatternEnabled=1
+# export cameraDumpPath=xxx
+ 
+# export GST_DEBUG=icamerasrc:7
