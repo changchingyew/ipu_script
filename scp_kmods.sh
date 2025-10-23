2be117eb830ab74ab85aab24ec6255b356d528a8
Yew, Chang Ching <chang.ching.yew@intel.com>
2025-10-24 06:41:13 +0800

    Update scp_kmods

--- scp_kmods.sh
+++ scp_kmods.sh
@@ -8,22 +8,21 @@ declare -a modules=(\
     # "drivers/media/i2c/isx031.ko" \
     # "drivers/i2c/i2c-atr.ko" \
     # "drivers/media/v4l2-core/v4l2*.ko"\
-    "drivers/media/v4l2-core/videodev.ko" \
+    # "drivers/media/v4l2-core/videodev.ko" \
     "drivers/media/i2c/maxim-serdes/max*.ko" \
-    "drivers/media/pci/intel/ipu-bridge.ko"
+    # "drivers/media/pci/intel/ipu-bridge.ko"
     )
 
 # export KERNEL_DIR=~/v4l2/repo/camera/kernel
 # export KERNEL_VER=6.12.34-vtg-gmsl
-# modules+=("*")
-# dirs+=("drivers/media/i2c/max9x")
 
 export KERNEL_DIR=~/v4l2/repo/camera/kernel-ml
-export KERNEL_VER=6.17.0-rc3-maxim-serdes-ml
+export KERNEL_VER=6.17.0-maxim-serdes-ml
 # sshpass -p $SSH_PASSWORD scp $KERNEL_DIR/arch/x86/boot/bzImage $SCP_TARGET:/boot/vmlinuz-$KERNEL_VER
 
 export SCP_TARGET_DIR=/usr/lib/modules/$KERNEL_VER/kernel
 
 for i in "${!modules[@]}"; do
-    sshpass -p $SSH_PASSWORD scp $KERNEL_DIR/${modules[$i]} $SCP_TARGET:$SCP_TARGET_DIR/${modules[$i]}
+    basepath=$(dirname "${modules[$i]}")
+    sshpass -p $SSH_PASSWORD scp $KERNEL_DIR/${modules[$i]} $SCP_TARGET:$SCP_TARGET_DIR/$basepath
 done
